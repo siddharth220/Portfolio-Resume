9e9b578ccac1928f9802d3e60a664ec945b8c0dd
Siddharth Harshit <harshit.siddharth56@gmail.com>
2023-09-09 11:56:36 +0530

    Add Loader

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,8 +2,8 @@ import AnimatedLetters from '../AnimatedLetters';
 import { useState, useEffect } from 'react';
 import './index.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCss3, faHtml5, faJava, faJsSquare, faNodeJs, faPython, faReact } from '@fortawesome/free-brands-svg-icons';
-import { faDatabase } from '@fortawesome/free-solid-svg-icons';
+import { faCss3, faHtml5, faJava, faJsSquare, faNodeJs, faReact } from '@fortawesome/free-brands-svg-icons';
+import Loader from 'react-loaders';
 
 const About = () => {
     const [letterClass, setletterClass] = useState('text-animate')
@@ -15,6 +15,7 @@ const About = () => {
       }, [])
 
     return (
+        <>
         <div className='container about-page'>
             <div className='text-zone'>
                 <h1>
@@ -46,18 +47,14 @@ const About = () => {
                             <FontAwesomeIcon icon={faJava} color='#F14C4D'/>
                         </div>
                         <div className='face6'>
-                            <FontAwesomeIcon icon={faPython} color='#FFE873'/>
-                        </div>
-                        <div className='face7'>
                             <FontAwesomeIcon icon={faNodeJs} color='#3C873A'/>
-                        </div>
-                        <div className='face8'>
-                            <FontAwesomeIcon icon={faDatabase} color='#00758F'/>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        <Loader type='pacman'/>
+        </>
     )
 }
 
